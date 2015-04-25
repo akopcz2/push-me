4bf74417aacdf0f79aaba8447576cbd401eec104
Androof <akopcz2@gmail.com>
2015-04-25 14:30:47 -0500

    Inital Push

---
+++ js/push.js
@@ -0,0 +1,18 @@
+function shunno_push_api() {
+  if (!Notification) {
+    alert('Hello World!'); 
+    return;
+  }
+
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+
+  var notification = new Notification('', {
+    icon: 'http://www.digitallofts.com/lofty.png',
+    body: "Say hello! hello@digitallofts.com",
+  });
+
+  notification.onclick = function () {
+    window.open("http://www.digitallofts.com");      
+  };
+}
