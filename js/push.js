916ec5b7c7a6369baa9a86a7b98972199310d7a3
Androof <akopcz2@gmail.com>
2015-04-25 19:19:12 -0500

    Pushing Changes

--- js/push.js
+++ js/push.js
@@ -16,3 +16,8 @@ function push_api() {
     window.open("http://www.digitallofts.com");      
   };
 }
+
+$('.wrap').mousemove(function(e) {
+    var x = (e.pageX * -1 / 16), y = (e.pageY * -1 / 16);
+    $(this).css('background-position', x + 'px ' + y + 'px');
+});
